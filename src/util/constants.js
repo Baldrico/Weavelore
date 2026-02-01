1e9c9351924843e371cf2e789bed77511288b6e7
Baldrico <118396547+Baldrico@users.noreply.github.com>
2026-02-01 14:57:33 +0000

    Update classes array to include 'witch'

--- src/util/constants.js
+++ src/util/constants.js
@@ -6,6 +6,6 @@ export default {
   srdVersion: 15,
   levels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
   schools: ['abjuration', 'conjuration', 'divination', 'enchantment', 'evocation', 'illusion', 'necromancy', 'transmutation'],
-  classes: ['bard', 'cleric', 'druid', 'paladin', 'ranger', 'sorcerer', 'warlock', 'wizard'],
+  classes: ['bard', 'cleric', 'druid', 'paladin', 'ranger', 'sorcerer', 'warlock', 'witch', 'wizard'],
   allowedTags: ['p', 'ul', 'li', 'table', 'thead', 'tbody', 'tr', 'th', 'td', 'i', 'b']
 }
